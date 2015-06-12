7cd15dfb83e72183f2ad179e7bf2c87fd2b1a717
Owen Nelson <onelson@laika.com>
2015-06-12 15:20:14 -0700

    resolving runtime errors re: thread state

--- core/src/main/scala/bbgun/BBGunApp.scala
+++ core/src/main/scala/bbgun/BBGunApp.scala
@@ -1,9 +1,15 @@
 package bbgun
 
 
+import java.util.concurrent.Executors
+
+import grizzled.slf4j.Logging
+
+import scala.concurrent.ExecutionContext
 import scalafx.Includes._
 import dispatch._, Defaults._
 import org.json4s._
+import scalafx.application.Platform
 import scalafx.geometry.Insets
 import scalafx.scene.layout.{GridPane, VBox, BorderPane}
 import scalafx.scene.text.Text
@@ -12,7 +18,15 @@ import scalafx.scene.text.Text
 case class Project(id: Option[Int], name: Option[String], description: Option[String], url: Option[String])
 
 
-class BBGunApp extends BorderPane {
+class BBGunApp extends BorderPane with Logging {
+//  implicit val ec = new ExecutionContext {
+//
+//    def execute(runnable: Runnable) {
+//      Platform.runLater(runnable)
+//    }
+//
+//    def reportFailure(t: Throwable): Unit = error(t)
+//  }
 
   implicit val formats = DefaultFormats
 
@@ -40,18 +54,21 @@ class BBGunApp extends BorderPane {
 
   for (resp <- Http(base.GET OK as.json4s.Json)) {
     // This code will execute whenever the Future resolves
-    val projects = (resp \ "items").extract[Seq[Project]]
 
-    projects.zipWithIndex.foreach {
-      case (proj: Project, idx: Int) =>
-        projectGrid.add(new Text(proj.name.getOrElse("")) {
-          prefWidth = 300
-        }, 0, idx)
-        projectGrid.add(new Text(proj.description.getOrElse("")) {
-          prefWidth = 1000
-        }, 1, idx)
-        projectGrid.add(new Text(proj.url.getOrElse("")), 2, idx)
+    Platform.runLater {  // wrap in a runLater() call to notify the main UI thread
+      val projects = (resp \ "items").extract[Seq[Project]]
+      projects.zipWithIndex.foreach {
+        case (proj: Project, idx: Int) =>
+          projectGrid.add(new Text(proj.name.getOrElse("")) {
+            prefWidth = 300
+          }, 0, idx)
+          projectGrid.add(new Text(proj.description.getOrElse("")) {
+            prefWidth = 1000
+          }, 1, idx)
+          projectGrid.add(new Text(proj.url.getOrElse("")), 2, idx)
+      }
     }
   }
+
 
 }
